1b6f43cec0425f4fbe9b6cfba69a48b2ece5d651
Bexruzbek-dev <behruz.toshpolatov14@gmail.com>
2024-05-29 17:23:50 +0500

    x

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:uzgaruvchi/controllers/main.dart';
+import 'package:uzgaruvchi/controllers/x.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
